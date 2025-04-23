ca61bdeb63812fde6e894e5941ee26fa6a864b25
Negel <timo.negel@cgi.com>
2025-04-23 21:18:42 +0200

    chore: set searchbar to navbar

---
+++ Pokemon/Components/PokemoStorage.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Models;
+
+namespace Pokemon.Components
+{
+    public static class PokemoStorage
+    {
+        public static List<PokemonModel> pokemons = [];
+
+        private static List<PokemonModel> searchedPokemons = [];
+        public static event Action? OnValueChange;
+
+        public static List<PokemonModel> SearchedPokemons
+        {
+            get => searchedPokemons;
+            set
+            {
+                searchedPokemons = value;
+                OnValueChange?.Invoke();
+            }
+        }
+    }
+}
